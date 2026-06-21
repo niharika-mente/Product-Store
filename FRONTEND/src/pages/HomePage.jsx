d9cc716a0e861d6d5236929bd5de60699f37bcb3
jyoti <jy26195@gmail.com>
2026-06-22 00:01:21 +0530

    Fix overlap between scroll to top and recently viewed buttons (#385)

--- FRONTEND/src/pages/HomePage.jsx
+++ FRONTEND/src/pages/HomePage.jsx
@@ -273,8 +273,8 @@ const HomePage = () => {
       {recentlyViewed.length > 0 && (
         <Button
           position="fixed"
-          bottom="20px"
-          right="20px"
+          bottom="16px"
+          right="70px"
           zIndex={99}
           colorScheme="teal"
           size="sm"
