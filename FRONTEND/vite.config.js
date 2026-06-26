bb0e01dc5a6f6e8574250fb460b20b07576e85e6
Aharshi3614 <aharshisinha2020@gmail.com>
2026-06-26 10:19:36 +0530

    fix: remove duplicate path import in vite.config

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -2,7 +2,6 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
